e805cff00ff2eda5a589f9612bb432a633c7d872
jaine716 <jaine716@snet.net>
2021-03-23 01:29:32 -0400

    working horizontal search

--- Project1.cpp
+++ Project1.cpp
@@ -51,6 +51,31 @@ using std::vector;
 //findMatch looks for word in the grid (2-d array) 
 //locations holds everywhere you find the word
 //word is word youre looking for
+
+int isUpper = 0;	//global variable, holds how many uppercase letters are in word that's being searched for
+
+string convertToLower(string word, int length){
+
+
+	string wordLower;		//holds lowercase version of word
+	char lower[length];		//array which holds individual characters that make up word (needed to evaluate case of each letter)
+
+	for(int y = 0; y < length; y++){	//convert the word we're searching for to an array of chars called lower[]
+		lower[y] = word.at(y);			//extract each character from word and put in lower[]
+	}
+
+		//create lowercase version of the word being searched for
+		for(int x = 0; x < length; x++){
+				
+			if(isupper(lower[x])){				//if the character is uppercase
+				lower[x] = tolower(lower[x]);	//convert to lowercase
+				isUpper++;						//keep track of how many uppercase letters have been found
+			}
+			wordLower = wordLower + lower[x];	//concatenate into new all-lowercase version of word
+	}
+
+	return wordLower;		//return lowercase version of word
+}
 	
 int findMatch(vector<Location *> * locations, char *letters, int size, string word) {
 /* In genericTests():
@@ -60,77 +85,66 @@ int findMatch(vector<Location *> * locations, char *letters, int size, string wo
 									{ 'a', 'c', 'g', 't' },
 									{ 'a', 'c', 't', 'g' } };				*/
 
-	int totalScore = 0;		//cumulative score
-	int count = 0;			//counts how many matches there are
-	int score = 0;			//individual word score
-	int line  = 0;			//line of a match
-	int col = 0;			//column of a match
+	//cout << "letters:" << letters << endl;
 
-	bool horizBool = false;		//true if match is horizontal, false if vertical
+	int totalScore = 0;			//cumulative score
+	int count = 0;				//counts how many matches there are
+	float score = 0;			//individual word score
+
+	bool horizBool = false;		//true if match is horizontal, false if vertical/no match
 	int length = word.size();	//size of the word we are searching for
+	int offset;					//offset of start of test word
 
-	char c;			//temporary character in for loop
-	string temp;	//temporary string in for loop
-
-	Location * loc = new Location();		//new dynamic object called loc, Location class which has members ptr, horizontal, & score
-
+	char c;						//temporary character in for loop
+	string temp;				//temporary string in for loop
+	string wordLower;			//holds lowercase version of the word being searched for
 	
+
 	cout << endl << "word: " << word << "   length: " << length << endl;
 
-	for(int i = 0; i < (size*size); i++){		//go through all 16 elements in letters (size=4)
+	for(int i = 0; i < size; i++){		//go through the rows
 
-		for(int j = 0; j < length; j++){
+		for(int j = 0; j < (size - length + 1); j++){	//go through the cols
 
-
-			if((i+j) > 15)		//don't go outside bounds of vector
-				break;
-					
-			c = letters[i+j];
-			temp = temp + c;
-
-		// letters[i] ==> if int i/4 has no remainder, you're in a new row
-		// I want to find a good way to stop the search from "running into" the next row
-		//	if( (remainder((i+j),size) == 0) && ((i+j) != 0))		
-		//		break;
-
-		}	//end nested for
-
-		if(temp == word){
-			cout << " match! ";
-
-			horizBool = true;
-			line = i/size;
-			score = 100;
-
-			//0,4,8,12 => column 0; 1,5,9,13 => column 1; 2,6,10,14 => column 2; 3,7,11,15 => column 3
-			if( (i == 0) || (i == 4) || (i == 8) || (i == 12) )
-				col = 0;
-			if ( (i == 1) || (i == 5) || (i == 9) || (i == 13) )
-				col = 1;
-			if ( (i == 2) || (i == 6) || (i == 10) || (i == 14) )
-				col = 2;
-			if ( (i == 3) || (i == 7) || (i == 11) || (i == 15) )
-				col = 3;
-			count++;
-
-			cout << "found at line:" << line << " column:" << col <<  " count: " << count << endl;
-
-			//if a match is found, save the (beginning of the) location of the match to loc->ptr
-			loc->ptr = &letters[ (line * size) + col ]; //this will 't'
-			//loc->ptr = &letters[i];
-			loc->horizontal = horizBool;	
-			loc->score = score;	
-
-			//cout << loc->ptr << " " << loc->horizontal << " " << loc->score << endl;
-
-			locations->push_back( loc );
-			totalScore += loc->score;
+			// form word
+			offset = (i * size) + j;			// starting position in letters
+			for(int k = 0; k < length; ++k){
+				c = letters[offset + k];		//extract a character c, from letters
+				temp = temp + c;				//concatenate c with temp, which holds a possible match for comparison
+																
+			}
 			
-		}	//end if statement
+			//get the lowercase version of the word being searched for
+			wordLower = convertToLower(word, length);
 
-		//cout << temp << "  *  ";		//use this to print out temp as the vector is searched
-		temp = "";						//reset temp string
-		
+			if((temp == word) || (temp == wordLower)){		//if temp is an exact match to word, or the all-lowercase version of word
+				cout << " horiz. match! ";
+				count++;
+
+
+				Location * loc = new Location();		//new dynamic object called loc
+				horizBool = true;						//horizontal match = true						
+
+				//score the match
+				score = (((100.0/length)/2.0) * isUpper) + ((100.0/length) * (length - isUpper));
+				
+				cout << "found at row:" << i << " column:" << j << " off:" << offset <<  " count: " << count << endl;
+				
+
+				//dynamic object loc holds address of a match, horizBool, and score
+				loc->ptr = &letters[offset];	//if a match is found, save the (beginning of the) location of the match to loc->ptr
+				loc->horizontal = horizBool;	//if a horizontal match is found, horizBool = true
+				loc->score = score;				//score of the individual match
+
+				locations->push_back( loc );	//push_back (add) another object, loc, to the locations vector
+				totalScore += loc->score;		//sum up all the scores to get a total score
+			}
+			temp = "";						//reset temp string
+			wordLower = "";					//reset lowercase word string
+			isUpper = 0;					//reset uppercase letter counter
+
+
+		}	//end for loop
 	}	//end for loop
 
 	cout << endl << "count: " << count << " total score: " << totalScore << endl << endl;
